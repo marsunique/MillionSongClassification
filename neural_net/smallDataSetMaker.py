aea5153cdfc7dc8fba64dcbae48d8438cdba9859
Dustin <lambright.dustin@gmail.com>
2017-04-06 15:06:22 -0400

    smaller nn dataset

--- neural_net/smallDataSetMaker.py
+++ neural_net/smallDataSetMaker.py
@@ -1,10 +1,18 @@
 import re
 
+
 totalCount = 0
 successfulCount = 0
+unsuccessfulCount = 0
+
+successfulOut = open("successful.csv", "w")
+unsuccessfulOut = open("unsuccessful.csv", "w")
+
 with open("millionSongs(labeled).csv", "r+") as inFile:
     
     for line in inFile:
+        if successfulCount > 100 and unsuccessfulCount > 100:
+            break
         totalCount = totalCount + 1
         splitLine = line.split(",")
         if totalCount % 1000 == 0:
@@ -12,6 +20,12 @@ with open("millionSongs(labeled).csv", "r+") as inFile:
         
         if "yes" in splitLine[len(splitLine)-1]:
             successfulCount = successfulCount + 1
+            successfulOut.write(line)
+        
+        elif unsuccessfulCount < 101:
+            unsuccessfulCount = unsuccessfulCount + 1
+            unsuccessfulOut.write(line)
+
 
 
 print "total:      " + str(totalCount)
